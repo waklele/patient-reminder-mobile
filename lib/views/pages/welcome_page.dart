bbc946c938f98f30d8efc1dea357d52c9f46919f
waklele <complexrangga@gmail.com>
2024-02-17 12:53:58 +0700

    add image and text to welcome page

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -7,6 +7,9 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const padding = 20.0;
+    const textStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
@@ -14,6 +17,34 @@ class WelcomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Container(
+              width: screenWidth - padding * 2,
+              height: screenWidth - padding * 2,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(padding)),
+                image: const DecorationImage(
+                  image: NetworkImage(
+                    "https://images.unsplash.com/photo-1649768518707-a5e513bd3fee?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 5,
+                    blurRadius: 15,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenWidth * .1),
+            const Text("Selamat Datang", style: textStyle),
+            Text(
+              "RS Batam",
+              style: textStyle.copyWith(color: Colors.grey),
+            ),
+            SizedBox(height: screenWidth * .1),
             RectangleButton(
               text: "Daftar",
               onPressed: () => Get.toNamed("/sign-up"),
