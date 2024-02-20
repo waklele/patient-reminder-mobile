5170cf94edef7a6b8080463b09bfd84d42532963
waklele <complexrangga@gmail.com>
2024-02-20 23:11:00 +0700

    Add a textbox to sign_up and add card to sign_up_finish

--- lib/views/pages/sign_up_finish_page.dart
+++ lib/views/pages/sign_up_finish_page.dart
@@ -4,7 +4,9 @@ import '../widgets/rectangle_button.dart';
 
 class SignUpFinishPage extends StatelessWidget {
   const SignUpFinishPage({super.key});
-
+  static const textStyle =
+      TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white);
+  static const padding = 20.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +16,41 @@ class SignUpFinishPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Card(
+              color: Colors.greenAccent.shade400,
+              margin: EdgeInsets.symmetric(vertical: 24.0, horizontal: 1.0),
+              child: Container(
+                decoration: new BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                        color: Colors.green.withOpacity(0.3),
+                        spreadRadius: 5,
+                        blurRadius: 15,
+                        offset: const Offset(0, 10)),
+                  ],
+                ),
+                height: 164.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.thumb_up_outlined,
+                        color: Colors.yellow,
+                        size: 54.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Akun berhasil dibuat',
+                        style: textStyle,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
             RectangleButton(
               text: "Masuk ke Beranda",
               backgroundColor: Colors.white,
--- lib/views/pages/sign_up_page.dart
+++ lib/views/pages/sign_up_page.dart
@@ -21,11 +21,154 @@ class SignUpPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.symmetric(horizontal: 14),
           child: Column(
             children: [
               Expanded(
-                child: ListView(),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 14),
+                    padding: const EdgeInsets.all(20),
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 0,
+                          blurRadius: 3,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          "Nama",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 6),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "Type a message...",
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            hintStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                            ),
+                            border: textFieldBorder,
+                            enabledBorder: textFieldBorder,
+                            focusedBorder: textFieldBorder.copyWith(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        const Text(
+                          "Email",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 6),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "Type a message...",
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            hintStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                            ),
+                            border: textFieldBorder,
+                            enabledBorder: textFieldBorder,
+                            focusedBorder: textFieldBorder.copyWith(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        const Text(
+                          "No. Handphone",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 6),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "Type a message...",
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            hintStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                            ),
+                            border: textFieldBorder,
+                            enabledBorder: textFieldBorder,
+                            focusedBorder: textFieldBorder.copyWith(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        const Text(
+                          "Password",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 6),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "*******",
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            hintStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                            ),
+                            border: textFieldBorder,
+                            enabledBorder: textFieldBorder,
+                            focusedBorder: textFieldBorder.copyWith(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
               RectangleButton(
                 text: "Selesai",
@@ -35,6 +178,13 @@ class SignUpPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  InputBorder get textFieldBorder {
+    return OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey.shade300),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
